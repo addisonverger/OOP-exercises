3e86dcca300ef59603825c05c68ea32836585e3c
Addison Verger <addison.j.verger@gmail.com>
2019-02-20 12:15:14 -0600

    person complete

---
+++ person.js
@@ -0,0 +1,20 @@
+class Person {
+  constructor(name, email, phone) {
+      this.name = name;
+      this.email = email;
+      this.phone = phone;
+  }
+  greet(otherPerson) {
+    console.log('Hello ' + otherPerson.name + ', I am ' + this.name + '!');
+  }
+}
+
+let sonny = new Person('Sonny', 'sonny@hotmail.com', '483-485-4948')
+
+let jordan = new Person('Jordan', 'jordan@aol.com', '495-586-3456')
+
+sonny.greet(jordan)
+jordan.greet(sonny)
+
+console.log('Contact Info: ' + sonny.email + ', ' + sonny.phone)
+console.log('Contact Info: ' + jordan.email + ', ' + jordan.phone)
